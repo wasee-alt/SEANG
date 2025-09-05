32df0c0f25609ca3b0cb28f432e1465b7ae56148
wasee-alt <wasee@proplugin.com>
2025-09-05 17:27:32 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from streamlit.components.v1 import html
 
 st.set_page_config(page_title="S.E.A.N.G • ProPlugin", layout="wide")
 
-# อ่านไฟล์ HTML เต็มหน้า (ที่อยู่ข้างๆ app.py)
 with open("index.html", "r", encoding="utf-8") as f:
     page = f.read()
 
-# ✅ ต้องใช้ components.html (ห้ามใช้ st.markdown/st.write กับ HTML เต็มหน้า)
+# Render full HTML app
 html(page, height=1100, scrolling=True)
